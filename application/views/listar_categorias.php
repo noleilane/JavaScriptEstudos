5f6977fecbfeadeaecf2d3f013aaca72c399e1f8
Leilane <53538519+noleilane@users.noreply.github.com>
2021-06-06 01:00:32 -0300

    Initial commit

---
+++ application/views/listar_categorias.php
@@ -0,0 +1,18 @@
+  
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Listar categorias</title>
+</head>
+<body>
+	<h1>Lista de categorias</h1>
+   <ul>
+      <li>Transporte</li>
+      <li>Alimentação</li>
+      <li>Remédios</li>
+      <li>Telefone/Internet</li>
+      <li>Outros gastos</li>
+   </ul> 
+</body>
+</html>
